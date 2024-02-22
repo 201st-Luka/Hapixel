58d7c3a001f73e642de23281bc6a35512c8f0789
201st-Luka <luka.eichten@gmail.com>
2024-02-22 15:55:37 +0100

    needed function for the Response class generation using a metaclass

--- hapixel/utils/functions.py
+++ hapixel/utils/functions.py
@@ -1,8 +1,20 @@
 from aiohttp import ClientSession
 
 
+__all__ = (
+    'uuid_from_username',
+    'snake_to_camel_case'
+)
+
+
 async def uuid_from_username(username: str) -> str:
     async with ClientSession() as session:
         async with session.get(f"https://api.mojang.com/users/profiles/minecraft/{username}") as response:
             data = await response.json()
             return data["id"]
+
+
+def snake_to_camel_case(string: str) -> str:
+    words = string.split('_')
+    return words[0] + ''.join(word.capitalize() for word in words[1:])
+
