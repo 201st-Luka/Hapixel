b759e3c116f9a27460725de9b2804a86c775880f
201st-Luka <luka.eichten@gmail.com>
2024-03-12 18:35:00 +0100

    improved error handling

--- hapixel/collections/__init__.py
+++ hapixel/collections/__init__.py
@@ -1,2 +1,15 @@
-from .enums import HapixelStatus, RequestType, Games, Servers
-from .exceptions import HapixelException, ApiKeyException, RequestNotDone, NoAuthenticationProvided
+from .enums import HapixelStatus, Games, Servers
+from .exceptions import (
+    HapixelException,
+    RequestNotDone,
+    NoAuthenticationProvided,
+    ApiException,
+    ApiKeyException,
+    MissingData,
+    InvalidData,
+    Forbidden,
+    NoData,
+    Throttled,
+    UnavailableData,
+)
+
